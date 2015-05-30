1692bc0e7552d418cbcdb9d9b1f35c459d485262
Sean Redmond <github-smr@sneakemail.com>
2015-05-30 15:30:51 -0400

    More documentation

--- lib/subjoin/links.rb
+++ lib/subjoin/links.rb
@@ -1,14 +1,34 @@
 module Subjoin
+  # A links object, i.e. a container for a number of link objects
+  #
+  # Example:
+  #
+  #     "links": {
+  #         "self": "http://example.com/posts",
+  #         "related": {
+  #             "href": "http://example.com/articles/1/comments",
+  #             "meta": {
+  #                 "count": 10
+  #             }
+  #         }
+  #     }
+  #
   class Links
+    # The array of {Link} objects
+    # @return [Array]
     attr_reader :links
     def initialize(data)
       @links = load_links(data)
     end
 
+    # Return the keys of all the contained links
+    # @return [Array]
     def keys
       @links.keys
     end
 
+    # Get a {Link} by key
+    # @return [Link]
     def [](k)
       @links[k]
     end
