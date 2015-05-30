21f94d58387fb2c4bb55f325efb368149f8727b7
Sean Redmond <github-smr@sneakemail.com>
2015-05-30 13:35:56 -0400

    Load linkages
    Load relationship linkages as Identifier objects

--- lib/subjoin/relationship.rb
+++ lib/subjoin/relationship.rb
@@ -2,9 +2,18 @@ module Subjoin
   class Relationship
     include Linkable
 
-    attr_reader :links
+    attr_reader :links, :linkages
     def initialize(data)
       @links = load_links(data['links'])
+      @linkages = load_linkages(data['data'])
     end
+
+    private
+    def load_linkages(data)
+      return [] if data.nil?
+      return [Identifier.new(data)] if data.is_a? Hash
+      data.map{|l| Identifier.new(l)}
+    end
+
   end
 end
--- spec/relationship_spec.rb
+++ spec/relationship_spec.rb
@@ -3,18 +3,52 @@ require "spec_helper"
 
 describe Subjoin::Relationship do
   before :each do
-    @r = Subjoin::Relationship.new(
+    @auths = Subjoin::Relationship.new(
       JSON.parse(ARTICLE)['data']['relationships']['author']
     )
   end
 
   it "is linkable" do
-    expect(@r.links).to be_an_instance_of(Subjoin::Links)
+    expect(@auths.links).to be_an_instance_of(Subjoin::Links)
   end
 
   it "has the right links" do
-    
-    expect(@r.links.keys).to eq ["self", "related"]
+    expect(@auths.links.keys).to eq ["self", "related"]
   end
-    
+
+  describe "#linkages" do
+    context "with a single linkage" do
+      it "returns an Array" do
+        expect(@auths.linkages).to be_an_instance_of Array
+      end
+
+      it "returns an array with a single member" do
+        expect(@auths.linkages.count).to eq 1
+      end
+        
+      it "returns an array of Identifier objects" do
+        expect(@auths.linkages.map{|l| l.class}.uniq).to eq [Subjoin::Identifier]
+      end
+    end
+
+    context "with an array of linkages" do
+      before :each do
+        @comments = Subjoin::Relationship.new(
+          JSON.parse(ARTICLE)['data']['relationships']['comments']
+        )
+      end      
+
+      it "returns an Array" do
+        expect(@comments.linkages).to be_an_instance_of Array
+      end
+
+      it "returns an array with more than 1 member" do
+        expect(@comments.linkages.count).to be > 1
+      end
+        
+      it "returns an array of Identifier objects" do
+        expect(@comments.linkages.map{|l| l.class}.uniq).to eq [Subjoin::Identifier]
+      end
+    end
+  end
 end
