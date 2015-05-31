29c301b4dc9c3df1b91b79b90d03b5a46187cb0b
Sean Redmond <github-smr@sneakemail.com>
2015-05-30 22:18:27 -0400

    Add documentation

--- lib/subjoin/key.rb
+++ lib/subjoin/key.rb
@@ -1,13 +1,22 @@
 module Subjoin
+  # Treats {#type} and {#id} as a composite key
   class Key
+    # The type attribute
     attr_reader :type
+
+    # The id attribute
     attr_reader :id
 
+    # @param [String] type The object type
+    # @param id The object id
     def initialize(type, id)
       @type = type
       @id = id
     end
 
+    # True if {#type} and {#id} are both equal
+    # @param [Key] other The Key to compare
+    # @return [Boolean]
     def ==(other)
       @type == other.type && @id == other.id
     end
