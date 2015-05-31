7f428deebc75debdec11aa8f00fcaebba93d8f61
Sean Redmond <github-smr@sneakemail.com>
2015-05-31 16:35:50 -0400

    Use Metable module
    Use Metable methods to load and access meta in Identifier and Link

--- lib/subjoin/identifier.rb
+++ lib/subjoin/identifier.rb
@@ -1,8 +1,10 @@
 module Subjoin
   class Identifier
     include Keyable
+    include Metable
     def initialize(data)
       load_key(data)
+      load_meta(data['meta'])
     end
   end
 end
--- lib/subjoin/link.rb
+++ lib/subjoin/link.rb
@@ -1,24 +1,19 @@
 module Subjoin
   # A link object
   class Link
+    include Metable
+
     # The URL for this link
     # @return String
     attr_reader :href
 
-    # Metadata object for this link
-    attr_reader :meta
     def initialize(data)
       if data.is_a? String
         @href = data
       else
         @href = data['href']
-        @meta = data['meta'].nil? ? nil : Subjoin::Meta.new(data['meta'])
+        load_meta(data['meta'])
       end
-    end
-
-    # @return True if the Link has a defined `meta` member
-    def has_meta?
-      return ! @meta.nil?
     end
 
     # Returns the {#href} attribute
