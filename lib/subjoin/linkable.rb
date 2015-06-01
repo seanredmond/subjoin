aa9cf3e567e2548fb6b68ea086c9ed89c7a81899
Sean Redmond <github-smr@sneakemail.com>
2015-05-31 23:46:42 -0400

    Allow for nil links attribute

--- lib/subjoin/linkable.rb
+++ lib/subjoin/linkable.rb
@@ -1,16 +1,16 @@
 module Subjoin
   # Generically construct and handle {Links} objects
   module Linkable
-    @links = nil
-
-    def links
-      @links ||= {}
-    end
-
+    attr_reader :links
+    
     # Load the object's links
     # @param data [Hash] The object's parsed JSON `links` member
     def load_links(data)
-      @links = Links.new(data)
+      if data.nil?
+        @links = nil if data.nil?
+      else
+        @links = Links.new(data)
+      end
     end
   end
 end
