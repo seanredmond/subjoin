1694645d07fee19d4f0326a9cbd6579582afb04d
Sean Redmond <github-smr@sneakemail.com>
2015-06-13 23:59:10 -0400

    Update tests
    The InheritableResource class has been changes to an Inheritable module

--- spec/inheritable_resource_spec.rb
+++ spec/inheritable_resource_spec.rb
@@ -1,7 +1,8 @@
 require "spec_helper"
 
 module Subjoin
-  class ExampleResource < Subjoin::InheritableResource
+  class ExampleResource < Resource
+    include Inheritable
     ROOT_URI="http://example.com"
   end
 
@@ -9,31 +10,19 @@ module Subjoin
     TYPE_PATH="nonstandard"
   end
 
-  class PoorlySubclassed < Subjoin::InheritableResource
-  end
-
   class ExampleArticle < ExampleResource
     TYPE_PATH="articles"
   end
 end
 
-describe Subjoin::InheritableResource do
+describe Subjoin::Inheritable do
   before :each do
     allow_any_instance_of(Faraday::Connection).
       to receive(:get).and_return(double(Faraday::Response, :body => ARTICLE))
-    @sub    = Subjoin::ExampleResource.new(URI("http://example.com/articles/1"))
-    @nonstd = Subjoin::NonStandardUri.new(URI("http://example.com/articles/1"))
-    @unsub  = Subjoin::Resource.new(URI("http://example.com/articles/1"))
-    @poor   = Subjoin::PoorlySubclassed.new(URI("http://example.com/articles/1"))
-    Subjoin::Document.new({})
   end
   
   it "has a root uri" do
     expect(Subjoin::ExampleResource::ROOT_URI).to eq "http://example.com"
-  end
-  
-  it "has a different class" do
-    expect(@sub.class).to eq Subjoin::ExampleResource
   end
   
   describe "#type_url" do
